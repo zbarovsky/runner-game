0639bc398fb8a23ae0728a1bdcd6e73ff5d87986
zbarovsky <zachary.barovsky@gmail.com>
2020-11-10 11:16:00 -0800

    third attempt at building a file structure

---
+++ RunnerGame/GlobalComponents/Movement.swift
@@ -0,0 +1,2 @@
+// global componenet to address background/enemey/object movement (basically all movement that isn't the player)
+
---
+++ RunnerGame/Player/Entities/Player.swift
@@ -0,0 +1,1 @@
+// here's where the stock player entity/class will be loaded
