47a8aac48d0eb2ba80ef45ecb07647de6c8a4b65
zbarovsky <zachary.barovsky@gmail.com>
2020-11-20 14:35:59 -0800

    Updated Object component to set up for spawning and movement capabilities. Created ObjectComponent to spawn objects. Removed unnecessary enemy counter file.

---
+++ RunnerGame/Components/ObjectComponent.swift
@@ -0,0 +1,21 @@
+//
+//  ObjectComponent.swift
+//  RunnerGame
+//
+//  Created by Zack Barovsky on 11/20/20.
+//
+
+import Foundation
+import SpriteKit
+import GameKit
+
+class ObjectComponent: GKComponent {
+    
+    override init() {
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
